593550243a40cba1851faa18199b610dda64537e
Leo Ip <149252202+ip-leo@users.noreply.github.com>
2024-11-07 03:04:18 +0000

    Add files via upload

---
+++ CathayApp/pages/personalcenter/personalcenter.js
@@ -0,0 +1,9 @@
+Page({
+  data: {
+    // Define any initial data for your page here
+  },
+  onLoad: function (options) {
+    // This function runs when the page is loaded
+    console.log("Personal Center Page Loaded");
+  }
+});
