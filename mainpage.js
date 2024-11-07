4ba8caceb14ed6d8dc312ea0d9b4a157d665f2d7
Leo Ip <149252202+ip-leo@users.noreply.github.com>
2024-11-07 02:55:00 +0000

    Create mainpage.js

---
+++ mainpage.js
@@ -0,0 +1,27 @@
+Page({
+ data: {
+   locations: ['上海 (SHA)', '香港 (HKG)'],
+   departure: '上海 (SHA)',
+   arrival: '香港 (HKG)',
+   date: '选择出发日期',
+   cabinClass: '经济舱',
+   passengers: '1成人 0儿童 0婴儿'
+ },
+ bindDepartureChange(e) {
+   this.setData({
+     departure: this.data.locations[e.detail.value]
+   });
+ },
+ bindArrivalChange(e) {
+   this.setData({
+     arrival: this.data.locations[e.detail.value]
+   });
+ },
+ bindDateChange(e) {
+   this.setData({
+     date: e.detail.value
+   });
+ },
+ searchFlights() {
+ }
+});
